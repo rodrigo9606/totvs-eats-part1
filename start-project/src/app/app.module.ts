e9261f0ed0d3d690f509fbf395d155b163c69608
Rodrigo Guimaraes Soares <rodrigo.gsoares@sp01.local>
2019-07-16 16:31:45 -0300

    Adicionando Rotas

--- start-project/src/app/app.module.ts
+++ start-project/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
